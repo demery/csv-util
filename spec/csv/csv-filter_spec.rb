d80a409adf65022ac619b1975b842d0ce5e69589
R. Douglas Emery <rdouglas.emery@icloud.com>
2023-01-15 16:04:17 -0500

    csv-filter: update usage; add --list-encodings and specs

---
+++ spec/csv/csv-filter_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+RSpec.describe 'csv-filter', :type => :aruba do
+
+  context 'when run with --help', :type => :aruba do
+    before { run_command 'csv-filter --help' }
+
+    it { expect(last_command_started).to have_output an_output_string_matching 'Usage: csv-filter' }
+  end
+
+  context 'when filtering by --text=fig', type: :aruba do
+    before { copy '%/unfiltered.csv', '.' }
+    let(:output) { read('%/csv-filter/efg-filtered-text.csv').join($/) }
+
+    before { run_command 'csv-filter --column f --text=fig unfiltered.csv' }
+
+    it { expect(last_command_started).to have_output output}
+  end
+
+  context 'when filtering by --regex', type: :aruba do
+    before { copy '%/unfiltered.csv', '.' }
+    let(:output) { read('%/csv-filter/efg-filtered-regex.csv').join($/) }
+
+    before { run_command 'csv-filter --column e --regex="^.*ea" unfiltered.csv' }
+
+    it { expect(last_command_started).to have_output output}
+  end
+end
