7bb1aca889ce5a06ccf71edca8f5b5df9b017a54
R. Douglas Emery <rdouglas.emery@icloud.com>
2023-06-10 01:03:38 -0400

    csv-cat tests without options

--- tests/test-csv-cat.sh
+++ tests/test-csv-cat.sh
@@ -8,59 +8,137 @@ export PATH=${script_dir}/../exe:$PATH
 
 tmpdir=${TMPDIR:-/tmp}/$$
 
+abc='a,b,c
+5,6,7
+3,4,5
+1,2,3
+3,4,5'
+
+bcd='b,c,d
+bat,cat,fat
+jar,car,tsar
+rug,bug,tug
+jar,car,tsar'
+
+efg='e,f,g
+egg,fig,grape
+elbow,foot,gut
+easel,floor,girder'
+
+abcbcd_concat='a,b,c,d
+5,6,7,
+3,4,5,
+1,2,3,
+3,4,5,
+,bat,cat,fat
+,jar,car,tsar
+,rug,bug,tug
+,jar,car,tsar'
+
+abcbcd_sort='a,b,c,d
+,bat,cat,fat
+,jar,car,tsar
+,jar,car,tsar
+,rug,bug,tug
+1,2,3,
+3,4,5,
+3,4,5,
+5,6,7,'
+
+abcbcd_uniq='a,b,c,d
+5,6,7,
+3,4,5,
+1,2,3,
+,bat,cat,fat
+,jar,car,tsar
+,rug,bug,tug'
+
+abcbcd_sort_uniq='a,b,c,d
+,bat,cat,fat
+,jar,car,tsar
+,rug,bug,tug
+1,2,3,
+3,4,5,
+5,6,7,'
+
+abcbcdefg_concat='a,b,c,d,e,f,g
+5,6,7,,,,
+3,4,5,,,,
+1,2,3,,,,
+3,4,5,,,,
+,bat,cat,fat,,,
+,jar,car,tsar,,,
+,rug,bug,tug,,,
+,jar,car,tsar,,,
+,,,,egg,fig,grape
+,,,,elbow,foot,gut
+,,,,easel,floor,girder'
+
+outfile=$(mktemp)
+
+setup() {
+  abc_csv=$(mktemp)
+  echo "${abc}" > ${abc_csv}
+
+  bcd_csv=$(mktemp)
+  echo "${bcd}" > ${bcd_csv}
+
+  efg_csv=$(mktemp)
+  echo "${efg}" > ${efg_csv}
+}
 
 test_concat_two_csvs() {
-  expected=$(cat ${fixtures}/abcbcd.csv)
-
-  actual=$(csv-cat ${fixtures}/abc.csv ${fixtures}/bcd.csv )
+  expected="${abcbcd_concat}"
+  actual=$(csv-cat ${abc_csv} ${bcd_csv} )
 
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_concat_two_csvs_sorted() {
-  expected=$(cat ${fixtures}/abcbcd-sort.csv)
-
-  actual=$(csv-cat --sort ${fixtures}/abc.csv ${fixtures}/bcd.csv )
+  expected="${abcbcd_sort}"
+  actual=$(csv-cat --sort ${abc_csv} ${bcd_csv} )
 
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_concat_two_csvs_uniq() {
-  expected=$(cat ${fixtures}/abcbcd-uniq.csv)
-
-  actual=$(csv-cat --uniq ${fixtures}/abc.csv ${fixtures}/bcd.csv )
+  expected="${abcbcd_uniq}"
+  actual=$(csv-cat --uniq ${abc_csv} ${bcd_csv} )
 
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_concat_two_csvs_sort_uniq() {
-  expected=$(cat ${fixtures}/abcbcd-sort-uniq.csv)
-
-  actual=$(csv-cat --sort --uniq ${fixtures}/abc.csv ${fixtures}/bcd.csv )
+  expected="${abcbcd_sort_uniq}"
+  actual=$(csv-cat --sort --uniq ${abc_csv} ${bcd_csv} )
 
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_concat_three_csvs() {
-  expected=$(cat ${fixtures}/abcbcdefg.csv)
-
-  actual=$(csv-cat ${fixtures}/abc.csv ${fixtures}/bcd.csv ${fixtures}/efg.csv )
+  expected="${abcbcdefg_concat}"
+  actual=$(csv-cat ${abc_csv} ${bcd_csv} ${efg_csv} )
 
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_pipe_two_csvs() {
-  expected=$(cat ${fixtures}/abcbcd.csv)
+  expected="${abcbcd_concat}"
+  file_list=$(mktemp)
+  printf "${abc_csv}\n${bcd_csv}" > ${file_list}
 
-  actual=$(ls ${fixtures}/abc.csv ${fixtures}/bcd.csv | csv-cat)
+  actual=$(csv-cat < ${file_list})
 
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_outfile_flag() {
-  outfile=${tmpdir}/abcbcd.csv
-  csv-cat -o $outfile ${fixtures}/abc.csv ${fixtures}/bcd.csv >/dev/null 2>&1
-  assert "test -e ${outfile}"
+  expected="${abcbcd_concat}"
+  outfile=${tmpdir}/abcbcd-csv$$
+
+  csv-cat -o ${outfile} ${abc_csv}  ${bcd_csv}  >/dev/null 2>&1
+  actual=$(cat ${outfile})
+  assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 
