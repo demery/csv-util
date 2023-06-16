24efbc105a51bc5b31b0a8eb2acd7be551a0c8f0
R. Douglas Emery <rdouglas.emery@icloud.com>
2023-06-15 22:49:19 -0400

    csv-cut test; remove fixture dependence

--- tests/test-csv-cut.sh
+++ tests/test-csv-cut.sh
@@ -7,51 +7,88 @@ export PATH=${script_dir}/../exe:$PATH
 
 tmpdir=${TMPDIR:-/tmp}/$$
 
+source_csv='first_col,second_col,third_col
+easel,floor,girder
+egg,fig,grape
+elbow,foot,gut
+eager,fickle,giddy
+steak,fig,cheese
+cucumber,fig,omelet'
 
 test_cut_one_column() {
-  expected=$(cat ${fixtures}/one_column.csv)
+  expected='floor
+fig
+foot
+fickle
+fig
+fig'
 
-  actual=$(${CSV_CUT} -c second_col ${fixtures}/source.csv)
-
+  actual=$(${CSV_CUT} -c second_col  <(echo "${source_csv}"))
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_cut_one_column_with_headers() {
-  expected=$(cat ${fixtures}/one_column_headers.csv)
+  expected='second_col
+floor
+fig
+foot
+fickle
+fig
+fig'
 
-  actual=$(${CSV_CUT} -c second_col --headers ${fixtures}/source.csv )
+  actual=$(${CSV_CUT} -c second_col --headers <(echo "${source_csv}") )
 
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_cut_two_columns() {
-  expected=$(cat ${fixtures}/two_columns.csv)
+  expected='easel,girder
+egg,grape
+elbow,gut
+eager,giddy
+steak,cheese
+cucumber,omelet'
 
-  actual=$(${CSV_CUT} -c first_col,third_col ${fixtures}/source.csv )
+  actual=$(${CSV_CUT} -c first_col,third_col  <(echo "${source_csv}"))
 
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_cut_two_columns_with_headers() {
-  expected=$(cat ${fixtures}/two_columns_headers.csv)
+  expected='first_col,third_col
+easel,girder
+egg,grape
+elbow,gut
+eager,giddy
+steak,cheese
+cucumber,omelet'
 
-  actual=$(${CSV_CUT} -c first_col,third_col --headers ${fixtures}/source.csv)
+  actual=$(${CSV_CUT} -c first_col,third_col --headers <(echo "${source_csv}"))
 
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_cut_two_columns_with_pipe_separator() {
-  expected=$(cat ${fixtures}/two_columns_tabs.csv)
+  expected='easel|girder
+egg|grape
+elbow|gut
+eager|giddy
+steak|cheese
+cucumber|omelet'
 
-  actual=$(${CSV_CUT} -c first_col,third_col -s '\|' ${fixtures}/source.csv )
+  actual=$(${CSV_CUT} -c first_col,third_col -s '\|' <(echo "${source_csv}"))
 
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_list_headers() {
-  expected=$(cat ${fixtures}/headers.txt)
+  expected='Headers in input:
+---
+first_col
+second_col
+third_col'
 
-  actual=$(${CSV_CUT} --list-headers ${fixtures}/source.csv)
+  actual=$(${CSV_CUT} --list-headers <(echo "${source_csv}"))
 
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
