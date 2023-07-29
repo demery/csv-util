ffa4cb8e839ce6045e40cd4eb29cc0774b915efc
R. Douglas Emery <rdouglas.emery@icloud.com>
2023-07-29 13:56:32 -0400

    csv-filter test: remove reliance on fixtures

--- tests/test-csv-filter.sh
+++ tests/test-csv-filter.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-fixtures=fixtures/csv-filter
-
-
-export PATH=${script_dir}/../exe:$PATH
-
-tmpdir=${TMPDIR:-/tmp}/$$
-
 unfiltered='first_col,second_col,third_col
 easel,floor,girder
 egg,fig,grape
@@ -22,7 +14,7 @@ egg,fig,grape
 steak,fig,cheese
 cucumber,fig,omelet'
 
-  actual=$(${CSV_FILTER} --column second_col --text fig <(echo "${unfiltered}")  )
+  actual=$(${CSV_FILTER} --column second_col --text fig <(echo "${unfiltered}") )
 
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
@@ -33,22 +25,9 @@ easel,floor,girder
 eager,fickle,giddy
 steak,fig,cheese'
 
-  actual=$(${CSV_FILTER} --column first_col --regex="^.*ea" ${fixtures}/unfiltered.csv  )
+  actual=$(${CSV_FILTER} --column first_col --regex="^.*ea" <(echo "${unfiltered}") )
 
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
-}
-
-setup_suite() {
-  mkdir ${tmpdir}
-}
-
-teardown_suite() {
-  rm_opts=-rf
-  if [[ -n ${CSVUTIL_VERBOSE} ]]
-  then
-    rm_opts="${rm_opts} -v"
-  fi
-  rm ${rm_opts} ${tmpdir}
 }
 
 CSV_FILTER="eval ../exe/csv-filter"
