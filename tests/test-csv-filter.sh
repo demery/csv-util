b68f8d8810c17b2b3bcbd2f96b60e3d64f913510
R. Douglas Emery <rdouglas.emery@icloud.com>
2023-07-29 09:22:28 -0400

    Update csv-filter specs and script to used fifos

--- tests/test-csv-filter.sh
+++ tests/test-csv-filter.sh
@@ -8,19 +8,34 @@ export PATH=${script_dir}/../exe:$PATH
 
 tmpdir=${TMPDIR:-/tmp}/$$
 
+unfiltered='first_col,second_col,third_col
+easel,floor,girder
+egg,fig,grape
+elbow,foot,gut
+eager,fickle,giddy
+steak,fig,cheese
+cucumber,fig,omelet'
 
 test_filter_by_text() {
+  expected='first_col,second_col,third_col
+egg,fig,grape
+steak,fig,cheese
+cucumber,fig,omelet'
+
   expected=$(cat ${fixtures}/filtered-text.csv)
 
-  actual=$(csv-filter --column second_col --text fig ${fixtures}/unfiltered.csv  )
+  actual=$(${CSV_FILTER} --column second_col --text fig <(echo "${unfiltered}")  )
 
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_filter_by_regex() {
-  expected=$(cat ${fixtures}/filtered-regex.csv)
+  expected='first_col,second_col,third_col
+easel,floor,girder
+eager,fickle,giddy
+steak,fig,cheese'
 
-  actual=$(csv-filter --column first_col --regex="^.*ea" ${fixtures}/unfiltered.csv  )
+  actual=$(${CSV_FILTER} --column first_col --regex="^.*ea" ${fixtures}/unfiltered.csv  )
 
   assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
@@ -37,3 +52,5 @@ teardown_suite() {
   fi
   rm ${rm_opts} ${tmpdir}
 }
+
+CSV_FILTER="eval ../exe/csv-filter"
