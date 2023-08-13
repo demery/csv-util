c4df2b78396a7f61a23f8ead6b96f013a7796e5e
R. Douglas Emery <rdouglas.emery@icloud.com>
2023-08-13 12:52:08 -0400

    Add  text/regex filter/reject tests

--- tests/test-csv-filter.sh
+++ tests/test-csv-filter.sh
@@ -4,8 +4,16 @@ unfiltered='first_col,second_col,third_col
 easel,floor,girder
 egg,fig,grape
 elbow,foot,gut
-eager,fickle,giddy
+eager,fickle,Giddy
 steak,fig,cheese
+cucumber,fig,omelet'
+
+unfiltered_with_empty_string='first_col,second_col,third_col
+easel,floor,girder
+egg,fig,grape
+elbow,,gut
+eager,fickle,Giddy
+steak,,cheese
 cucumber,fig,omelet'
 
 
@@ -27,7 +35,7 @@ test_filter_by_text_reject() {
   expected='first_col,second_col,third_col
 easel,floor,girder
 elbow,foot,gut
-eager,fickle,giddy'
+eager,fickle,Giddy'
 
   actual=$(${CSV_FILTER} --column second_col --text fig --reject <(echo "${unfiltered}") )
 
@@ -35,27 +43,69 @@ eager,fickle,giddy'
 }
 
 test_filter_by_case_insensitive_text() {
-  assert false
+  expected='first_col,second_col,third_col
+egg,fig,grape
+steak,fig,cheese
+cucumber,fig,omelet'
+
+  actual=$(${CSV_FILTER} --column second_col --text FIG --case-insensitive <(echo "${unfiltered}") )
+
+  assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_filter_reject_by_case_insensitive_text() {
-  assert false
+  expected='first_col,second_col,third_col
+easel,floor,girder
+elbow,foot,gut
+eager,fickle,Giddy'
+
+  actual=$(${CSV_FILTER} --column second_col --text fig --case-insensitive --reject <(echo "${unfiltered}") )
+
+  assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_filter_by_empty_string_text() {
-  assert false
+    expected='first_col,second_col,third_col
+elbow,,gut
+steak,,cheese'
+
+  actual=$(${CSV_FILTER} --column second_col --text \'\' <(echo "${unfiltered_with_empty_string}") )
+
+  assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_filte_reject_by_empty_string_text() {
-  assert false
+  expected='first_col,second_col,third_col
+easel,floor,girder
+egg,fig,grape
+eager,fickle,Giddy
+cucumber,fig,omelet'
+
+  actual=$(${CSV_FILTER} --column second_col --text \'\' --reject <(echo "${unfiltered_with_empty_string}") )
+
+  assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_filter_by_text_when_column_value_is_empty_string() {
-  assert false
+  expected='first_col,second_col,third_col
+egg,fig,grape
+cucumber,fig,omelet'
+
+  actual=$(${CSV_FILTER} --column second_col --text fig <(echo "${unfiltered_with_empty_string}") )
+
+  assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_filter_reject_by_text_when_column_value_is_empty_string() {
-  assert false
+  expected='first_col,second_col,third_col
+easel,floor,girder
+elbow,,gut
+eager,fickle,Giddy
+steak,,cheese'
+
+  actual=$(${CSV_FILTER} --column second_col --text fig --reject <(echo "${unfiltered_with_empty_string}") )
+
+  assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 
@@ -65,7 +115,7 @@ test_filter_reject_by_text_when_column_value_is_empty_string() {
 test_filter_by_regex() {
   expected='first_col,second_col,third_col
 easel,floor,girder
-eager,fickle,giddy
+eager,fickle,Giddy
 steak,fig,cheese'
 
   actual=$(${CSV_FILTER} --column first_col --regex="^.*ea" <(echo "${unfiltered}") )
@@ -85,27 +135,67 @@ cucumber,fig,omelet'
 }
 
 test_filter_by_case_insensitive_regex() {
-  assert false
+  expected='first_col,second_col,third_col
+easel,floor,girder
+eager,fickle,Giddy
+steak,fig,cheese'
+
+  actual=$(${CSV_FILTER} --column first_col --regex="^.*EA" --case-insensitive <(echo "${unfiltered}") )
+
+  assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_filter_reject_by_case_insensitive_regex() {
-  assert false
+  expected='first_col,second_col,third_col
+egg,fig,grape
+elbow,foot,gut
+cucumber,fig,omelet'
+
+  actual=$(${CSV_FILTER} --column first_col --regex="^.*eA" --reject --case-insensitive <(echo "${unfiltered}") )
+
+  assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_filter_by_empty_regex() {
-  assert false
+  expected='first_col,second_col,third_col
+elbow,,gut
+steak,,cheese'
+
+  actual=$(${CSV_FILTER} --column second_col --regex="^$" <(echo "${unfiltered_with_empty_string}") )
+  assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_filter_reject_by_empty_regex() {
-  assert false
+  expected='first_col,second_col,third_col
+easel,floor,girder
+egg,fig,grape
+eager,fickle,Giddy
+cucumber,fig,omelet'
+
+  actual=$(${CSV_FILTER} --column second_col --regex="^$" --reject <(echo "${unfiltered_with_empty_string}") )
+  assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_filter_by_regex_when_column_value_is_empty_string() {
-  assert false
+    expected='first_col,second_col,third_col
+egg,fig,grape
+eager,fickle,Giddy
+cucumber,fig,omelet'
+
+  actual=$(${CSV_FILTER} --column second_col --regex="^fi" <(echo "${unfiltered_with_empty_string}") )
+
+  assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 test_filter_reject_by_regex_when_column_value_is_empty_string() {
-  assert false
+  expected='first_col,second_col,third_col
+easel,floor,girder
+elbow,,gut
+steak,,cheese'
+
+  actual=$(${CSV_FILTER} --column second_col --regex="^fi" --reject <(echo "${unfiltered_with_empty_string}") )
+
+  assert_equals "${expected}" "${actual}" 'Unexpected CSV output'
 }
 
 
@@ -115,7 +205,7 @@ test_filter_reject_by_regex_when_column_value_is_empty_string() {
 test_pipe_to_csv_filter() {
     expected='first_col,second_col,third_col
 easel,floor,girder
-eager,fickle,giddy
+eager,fickle,Giddy
 steak,fig,cheese'
 
   actual=$(echo "${unfiltered}" | ${CSV_FILTER} --column first_col --regex="^.*ea")
